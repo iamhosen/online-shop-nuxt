5bac2d10620cc4754a2a446eb6448d26d9854a24
iamhosen <hamirhosseini3@gmail.com>
2024-03-28 17:58:05 +0330

    config: Setup tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,17 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 
-	modules: ['nuxt-icon'],
+	modules: ['nuxt-icon', '@nuxtjs/tailwindcss'],
 
 	runtimeConfig: {
 		public: {
 			API_BASE_URL: process.env.API_BASE_URL,
 		},
 	},
+
+	tailwindcss: {
+        cssPath: '~/assets/scss/app.scss',
+        configPath: 'tailwind.config',
+        viewer: true
+    },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <script lang="ts" setup>
-import {type Product} from '@/repository/modules/product';
+import { type Product } from "@/repository/modules/product";
 
 const { $api } = useNuxtApp();
 const products = ref<Product[]>([]);
 
-const response = await $api.product.index()
+const response = await $api.product.index();
 products.value = response;
 </script>
 
 <template>
   <Title>Home</Title>
 
-  Home Page <Icon name="mdi:home" />
+  <h1 class="text-xl font-bold">Home Page</h1>
+  <Icon name="mdi:home" />
   {{ products }}
 </template>
